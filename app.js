7fc50e4b38af9296556f6d629b1746a9e4fbb334
Rob Vanarsdall <rvanar@gmail.com>
2023-02-22 17:15:59 -0500

    updated to make use of new endpoint.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let form = document.querySelector(".form-container")
+let form = document.querySelector(".form-container");
 // let email = document.getElementById("email")
 // let pwd = document.getElementById("pwd")
 
-let db = []
+let db = [];
 
 // form.addEventListener("submit", event => {
 //     // prevents form default behavior of:
 //     // page refresh & http being sent
 //     event.preventDefault()
-    
+
 //     // Seed the makeshift db with object literal
 //     // The object has properties of email and password
 //     // The values are values inputted by the client into the form
@@ -26,11 +26,11 @@ let db = []
 // })
 
 // ? URL Query Parameters
-let url = document.location.search
-console.log("URL String", url)
-let params = new URLSearchParams(url)
+let url = document.location.search;
+console.log("URL String", url);
+let params = new URLSearchParams(url);
 // Creates a Location object that sanitizes query parameters
-let email = params.get("email")
+let email = params.get("email");
 // Utilize .get() method of the Location interface to access query values
-let pwd = params.get("pwd")
-console.log(email, pwd)
+let pwd = params.get("pwd");
+console.log(email, pwd);
---
+++ server.js
@@ -0,0 +1,40 @@
+let form = document.querySelector(".form-container");
+
+form.addEventListener("submit", async (event) => {
+  // prevents form default behavior of:
+  // page refresh & http being sent
+  event.preventDefault();
+  try {
+    const name = event.target[0].value;
+    const email = event.target[1].value;
+    const password = event.target[2].value;
+
+    //   ! Define the url that you are trying to reach
+    const url = "http://127.0.0.1/user/register";
+    //   ! Define the headers
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    //   ! Create a variable called data: you will need to stringify the data
+
+    const data = JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+    });
+
+    //   ! Create our request options
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: data,
+    };
+
+    // ! Conduct the Fetch
+    const response = await fetch(url, requestOptions);
+    const json = await response.json();
+
+    console.log(json);
+  } catch (err) {
+    console.error(err);
+  }
+});

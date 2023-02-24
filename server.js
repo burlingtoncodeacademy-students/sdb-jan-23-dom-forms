ce8a20244116036eea53dfa874bf45ce1a561ee1
Rob Vanarsdall <rvanar@gmail.com>
2023-02-23 19:18:21 -0500

    connecting to server

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ let form = document.querySelector(".form-container");
 form.addEventListener("submit", async (event) => {
   // prevents form default behavior of:
   // page refresh & http being sent
-  event.preventDefault();
   try {
+    event.preventDefault();
     const name = event.target[0].value;
     const email = event.target[1].value;
     const password = event.target[2].value;
 
     //   ! Define the url that you are trying to reach
-    const url = "http://127.0.0.1/user/register";
+    const url = "http://127.0.0.1:4000/user/register";
     //   ! Define the headers
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
